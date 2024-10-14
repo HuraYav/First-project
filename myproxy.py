b1c625cbdcd9cb0f0066bc6578d844a411e97c32
HuraYav <153281291+HuraYav@users.noreply.github.com>
2024-10-14 19:21:26 +0400

    Update myproxy.py

--- myproxy.py
+++ myproxy.py
@@ -1,13 +1,6 @@
-import sys
 import os
 
-def main():
-    username = "ad91234"
-    try:
-        password_proxy = raw_input("write your pass")
-    except:
-        password_proxy = input("write your pass")
-    
+def create_proxy(count, username, password):
     # Установка необходимых пакетов
     os.system("apt-get update")
     os.system("apt-get -y install build-essential libwrap0-dev libpam0g-dev libkrb5-dev libsasl2-dev")
@@ -15,15 +8,13 @@ def main():
     os.system("tar -xvpzf dante.tgz")
     os.system("apt-get -y install libwrap0 libwrap0-dev")
     os.system("apt-get -y install gcc make")
-    os.system("mkdir /home/dante")
+    os.system("mkdir -p /home/dante")
     os.system("cd dante && ./configure --prefix=/home/dante && make && make install")
 
     # Создание базового конфигурационного файла Dante
     conf = """
 logoutput: syslog /var/log/danted.log
-internal: eth0
-external: eth0
-
+external: ens3
 socksmethod: username
 user.privileged: root
 user.unprivileged: nobody
@@ -41,45 +32,28 @@ socks pass {
 }
 """
 
-    # Добавляем 1000 прокси с разными портами
+    # Генерация конфигурации для заданного количества прокси
     with open("/home/dante/danted.conf", "w") as f:
         f.write(conf)
-        for port in range(1080, 2079):  # Порты от 1080 до 2079
-            f.write(f"internal: eth0 port = {port}\n")
-    
-    # Создание пользователя
-    os.system("useradd --shell /usr/sbin/nologin -m %s" % username)
-    os.system('echo "%s:%s" | chpasswd' % (username, password_proxy))
-    
+        for port in range(1080, 1080 + count):  # Порты от 1080 до (1080 + count)
+            f.write(f"internal: ens3 port = {port}\n")
+
+    # Создание пользователя с заданным логином и паролем
+    os.system(f"useradd --shell /usr/sbin/nologin -m {username}")
+    os.system(f'echo "{username}:{password}" | chpasswd')
+
     # Настройка фаервола
     os.system("apt-get -y install ufw")
-    os.system("ufw status")
     os.system("ufw allow ssh")
-    os.system("ufw allow proto tcp from any to any port 1080:2079")
-    os.system("ufw status numbered")
-    os.system("echo 'y' | ufw enable")
-
-    # Добавление в автозапуск
-    os.system("""
-echo '#!/bin/sh -e
-sleep 20
-/home/dante/sbin/sockd -f /home/dante/danted.conf -D
-exit 0
-' > /etc/rc.local
-""")
-    os.system("chmod +x /etc/rc.local")
-    os.system("chmod +x /home/dante/sbin/sockd")
+    os.system(f"ufw allow proto tcp from any to any port 1080:{1080 + count - 1}")
+    os.system("ufw enable")
 
     # Запуск прокси
     os.system("/home/dante/sbin/sockd -f /home/dante/danted.conf -D")
-    os.system("echo 'proxy install success'")
-    os.system("echo ' '")
-    os.system("echo '________________________________'")
-    os.system("echo ' '")
-    os.system("echo \"YOUR IP ADDRESS: `hostname -I | awk '{print $1}'`\"")
-    os.system("echo 'PORT RANGE: 1080-2079'")
-    os.system("echo 'LOGIN: %s'" % username)
-    os.system("echo 'PASSWORD: %s'" % password_proxy)
+    print("Прокси-серверы созданы и запущены!")
 
 if __name__ == "__main__":
-    main()
+    count = int(input("Введите количество прокси для создания: "))
+    username = input("Введите имя пользователя : ")
+    password = input("Введите пароль : ")
+    create_proxy(count, username, password)
